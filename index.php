f8735f77f4b759ff7b1c7cb3b9d85fdc5d4a2a11
tonyinuma <tony.inuma@gmail.com>
2019-03-23 13:20:02 -0500

    Ahora el index.php fue modificado

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-foreach ($arreglo as $element) {
-	echo"$element, ";
-}
-echo'<br> decirlo al revés lo dudo <br>';
-foreach (array_reverse($arreglo) as $element) {
-	echo"$element, ";
-}
-echo'<br> ¡Qué trabajo me ha costado!';
+  echo("Hola mundo");
 
-
-?>
+ ?>
